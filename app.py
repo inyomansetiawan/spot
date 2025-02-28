55a79688f301861d3b998b102ef445fc3746cff0
inyomansetiawan <insetiawan98@gmail.com>
2025-02-28 18:24:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def export_pdf(data, filename):
 
         # Pisahkan teks berdasarkan newline
         if isinstance(value, str) and "\n" in value:
-            bullet_items = [ListItem(Paragraph(line.strip())) for line in value.split("\n") if line.strip()]
+            bullet_items = [ListItem(line.strip()) for line in value.split("\n") if line.strip()]
             answer = ListFlowable(bullet_items, bulletType="bullet", leftIndent=20)  # Indentasi rapi
         else:
             answer = str(value)
